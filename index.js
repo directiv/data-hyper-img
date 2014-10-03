18a285ecbef12e31d7dbc4186f9d50fa1e83aa98
camshaft <bytheway.cameron@gmail.com>
2014-10-02 23:34:02 -0600

    update the interface

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-/** @directiv data-stateless */
+/**
+ * Module dependencies
+ */
 
-exports.requires = ['hyper-store'];
+hyperImg.requires = ['store-hyper'];
 
-exports.exposes = [
-  'data-hyper-img',
-  'data-hyper-image',
-  'data-src'
-];
+/**
+ * Expose the 'hyper-img' directive
+ */
 
-exports.compile = function(input) {
-  var path = input.split('.');
-  return {
-    path: input,
-    target: path[path.length - 1]
+module.exports = hyperImg;
+
+/**
+ * Initialize the 'hyper-img' directive
+ *
+ * @param {StoreHyper} store
+ */
+
+function hyperImg(store) {
+  this.compile = function(input) {
+    var path = input.split('.');
+    return {
+      path: input,
+      target: path[path.length - 1]
+    };
   };
-};
 
-exports.state = function(config, state) {
-  var res = this('hyper-store').get(config.path, state.get());
-  if (!res.completed) return false;
-  return state.set(config.target, res.value);
-};
-
-exports.props = function(config, state, props) {
-  var image = state.get(config.target);
-  if (!image || !image.src) return props;
-  var commit = {
-    src: {$set: image.src}
+  this.state = function(config, state) {
+    var res = store.get(config.path, state);
+    if (!res.completed) return false;
+    return state.set(config.target, res.value);
   };
-  if (image.title) commit.title = {
-    $set: image.title
-  }
-  return props.set(commit);
-};
+
+  this.props = function(config, state, props) {
+    var image = state.get(config.target);
+    if (!image || !image.src) return props;
+    if (image.title) props = props.set('title', image.title);
+    return props.set('src', image.src);
+  };
+}
